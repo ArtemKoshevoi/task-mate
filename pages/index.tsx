376fd4b8cb6474742db33d690dc346f9706765dc
2mcKosh <2mcKosh@MacBook-Pro.local>
2021-07-07 16:21:47 +0300

    feat(app): add styles and graphql front generating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,27 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { gql, useQuery } from '@apollo/client';
+import React from 'react';
+import Head from 'next/head';
 import { initializeApollo } from '../lib/client';
-
-const TasksQueryDocument = gql`
-  query Tasks {
-    tasks {
-      id
-      title
-      status
-    }
-  }
-`;
-
-interface TasksQuery {
-  tasks: {
-    id: number;
-    title: string;
-    status: string;
-  }[]
-}
+import { TasksDocument, TasksQuery, useTasksQuery } from '../generated/graphql-frontend';
+import TaskList from '../components/TaskList';
 
 export default function Home() {
-  const result = useQuery<TasksQuery>(TasksQueryDocument);
+  const result = useTasksQuery();
   const tasks = result.data?.tasks;
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Tasks</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {tasks && tasks.length > 0 && tasks.map((task) => {
-        return (
-          <div key={task.id}>
-            {task.title} ({task.status})
-          </div>
-        )
-      })}
+      {result.loading ? (
+      <p>Loading tasks...</p>
+      ) : result.error ? (
+      <p>An error occurred.</p>
+      ) : tasks && tasks.length > 0 ? (
+      <TaskList tasks={tasks}/>
+      ) : (
+      <p className='no-tasks-message'>You've got no tasks.</p>
+      )}
     </div>
   )
 }
@@ -45,7 +30,7 @@ export const getStaticProps = async () => {
   const apolloClient = initializeApollo();
 
   await apolloClient.query<TasksQuery>({
-    query: TasksQueryDocument,
+    query: TasksDocument,
   });
 
   return {
